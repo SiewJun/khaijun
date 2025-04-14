eb19355e934727a971be575922b41498059ca852
SiewJun <siewkhaijun57@gmail.com>
2025-04-14 16:24:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NavBar from '@/components/nav-bar.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <NavBar />
+  <RouterView />
+</template>
---
+++ src/components/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li>Projects</li>
+      <li>Experiences</li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
