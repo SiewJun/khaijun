f6072a1a8be9b73e8af4bd4a3e16cc8e3f4f3fae
bitlab-khaijunsiew <siew.khaijun@thebitlab.co>
2025-07-07 15:58:54 +0800

    refactor: use switch cycle button

--- src/components/theme-toggle.vue
+++ src/components/theme-toggle.vue
@@ -1,50 +1,35 @@
 <script setup lang="ts">
 import { Sun, Moon, Laptop } from 'lucide-vue-next'
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { useThemeStore } from '@/stores/theme'
 import { onMounted } from 'vue'
 
 const themeStore = useThemeStore()
 
-// Initialize the theme when component mounts
+const themes = ['light', 'dark', 'system'] as const
+
+function nextTheme() {
+  const idx = themes.indexOf(themeStore.theme)
+  const next = themes[(idx + 1) % themes.length]
+  themeStore.setTheme(next)
+}
+
 onMounted(() => {
   themeStore.init()
 })
 </script>
 
 <template>
-  <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button variant="ghost" size="icon" class="relative size-9 rounded-full">
-        <Sun v-if="themeStore.theme === 'light'" class="h-[1.2rem] w-[1.2rem]" aria-hidden="true" />
-        <Moon v-if="themeStore.theme === 'dark'" class="h-[1.2rem] w-[1.2rem]" aria-hidden="true" />
-        <Laptop
-          v-if="themeStore.theme === 'system'"
-          class="h-[1.2rem] w-[1.2rem]"
-          aria-hidden="true"
-        />
-        <span class="sr-only">Toggle theme</span>
-      </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent>
-      <DropdownMenuItem @click="themeStore.setTheme('light')">
-        <Sun class="mr-2 h-4 w-4" />
-        <span>Light</span>
-      </DropdownMenuItem>
-      <DropdownMenuItem @click="themeStore.setTheme('dark')">
-        <Moon class="mr-2 h-4 w-4" />
-        <span>Dark</span>
-      </DropdownMenuItem>
-      <DropdownMenuItem @click="themeStore.setTheme('system')">
-        <Laptop class="mr-2 h-4 w-4" />
-        <span>System</span>
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+  <Button
+    variant="ghost"
+    size="icon"
+    class="relative size-9 rounded-full"
+    @click="nextTheme"
+    :aria-label="`Switch theme (current: ${themeStore.theme})`"
+  >
+    <Sun v-if="themeStore.theme === 'light'" class="h-[1.2rem] w-[1.2rem]" aria-hidden="true" />
+    <Moon v-if="themeStore.theme === 'dark'" class="h-[1.2rem] w-[1.2rem]" aria-hidden="true" />
+    <Laptop v-if="themeStore.theme === 'system'" class="h-[1.2rem] w-[1.2rem]" aria-hidden="true" />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
 </template>
